e3af7ccf3ba5142e6c1a7faf185b06d15a037151
xiaobingdu <308866567@qq.com>
2023-09-08 20:54:44 +0800

    测试

---
+++ graph/849-D1.cpp
@@ -0,0 +1,54 @@
+﻿#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 510; // 最大节点数
+int n, m;
+int g[N][N];
+int dist[N];
+bool st[N];
+
+int dijkstra()
+{
+    memset(dist, 0x3f, sizeof dist);
+    dist[1] = 0;
+    // 找队列头节点中的最短边head-t
+    // 以此最短边更新后继节点的最短路
+    // 邻接矩阵可以遍历全部直接加,邻接表则只更新t节点的后继
+    for (int i = 0; i < n - 1; i++) // 循环n-1次
+    {
+        int t = -1;
+        for (int j = 1; j <= n; j++)                      // 起点为1
+            if (!st[j] && (t == -1 || dist[t] > dist[j])) // 在j没有访问过条件下,一个中转节点只能用一次
+                t = j;                                    // t未被赋值或有权值更小的边出现更新t为j
+
+        for (int j = 1; j <= n; j++) // 以t为中间点,更新i-t-j的路程
+            dist[j] = min(dist[j], dist[t] + g[t][j]);
+
+        st[t] = true;
+    }
+
+    if (dist[n] == 0x3f3f3f3f)
+        return -1;
+    return dist[n];
+}
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+
+    memset(g, 0x3f, sizeof g);
+    while (m--)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+
+        g[a][b] = min(g[a][b], c);
+    }
+
+    printf("%d\n", dijkstra());
+
+    return 0;
+}
---
+++ graph/B3644.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include<unordered_map>
+using namespace std;
+typedef long long ll;
+const long long N = 111;
+//拓扑排序
+ll n;
+unordered_map<ll, unordered_map<ll, ll>> g;
+//入度为0的先输出，然后子节点入度都-1
+void bfs()
+{
+	queue<int>q;
+	for (int i = 1; i <= n; i++)
+	{
+		if (g[i].empty())
+		{
+			q.push(i);
+			cout << i << ' ';
+		}
+	}
+	while (!q.empty())
+	{
+		int i = q.front();
+		ll size = g[i].size();
+		q.pop();
+		for (auto it : g[i])
+		{
+			int j = it.first;
+			if (g[j].size() == size + 1)
+			{
+				q.push(j);
+				cout << j << ' ';
+			}
+		}
+	}
+	return ;
+}
+int main()
+{
+	freopen("C:\\Users\\30886\\Desktop\\in.txt", "r", stdin);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	int x;
+	for (int i = 1; i <= n; i++)
+	{
+		while (cin >> x)
+		{
+			if (x == 0 )break;
+			g[x][i] = 1;//反向建边
+		}
+	}
+	bfs();
+	return 0;
+}
